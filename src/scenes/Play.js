eb28320d2e4f3ce4d1898daec2c6bbc799ecc92a
Jarrett-Mao <74739111+Jarrett-Mao@users.noreply.github.com>
2021-04-27 16:48:56 -0700

    added barebones code to fill in. + jumping key

---
+++ src/scenes/Play.js
@@ -0,0 +1,25 @@
+class Play extends Phaser.Scene {
+    constructor(){
+        super("playScene");
+    }
+
+    preload() {
+        
+    }
+
+    create(){
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    }
+
+    update(){
+        // this.starfield.tilePositionX -= 4; //replace with actual background
+
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)){
+            //may need to be moved to player class
+            //fill code with jump
+            console.log("jumping!"); 
+        }
+
+    }
+}
