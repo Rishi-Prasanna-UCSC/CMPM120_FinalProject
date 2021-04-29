430311efff8e5c83c5d54fa6e19aa250fa6a2f07
“Rishi <riprasan@ucsc.edu>
2021-04-29 11:46:11 -0700

    Accepted change in programming

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,10 +2,6 @@ class Menu extends Phaser.Scene {
     constructor(){
         super("menuScene");
     }
-<<<<<<< HEAD
-    // Blablabla 
-=======
->>>>>>> 5a84df85f147c9aaebb991c98a82c6bd9bf99b98
     preload(){
 
     }
