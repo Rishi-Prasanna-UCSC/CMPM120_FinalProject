b4a52164c5b20d600a4afddf95dbd99ce2990d75
“Rishi <riprasan@ucsc.edu>
2021-04-30 20:43:13 -0700

    Main Menu complete - for now

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,11 +4,15 @@ class Menu extends Phaser.Scene {
     }
     preload(){
         this.load.image('MMBackground', '../../assets/MainMenuBackground.png');
+        this.load.image('PlayButton', '../../assets/PlayButton.png');
+        this.load.image('CreditsButton', '../../assets/CreditsButton.png');
     }
 
     create(){
         this.add.image(390, 220, 'MMBackground');
         //this.scene.start("playScene");
+        this.add.image(640, 100, 'PlayButton');
+        this.add.image(640, 250, 'CreditsButton');
     }
     update() {
 
