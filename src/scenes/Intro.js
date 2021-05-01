01cdfcdd00dfd7de07225d92dc9968592cb9275a
“Rishi <riprasan@ucsc.edu>
2021-04-30 22:16:26 -0700

    Intro Screen Optimized!

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -1,29 +1,48 @@
 class Intro extends Phaser.Scene {
     constructor(){
         super("introScene");
+        this.play = false;
     }
 
     preload() {
-
+        this.load.audio('Text', '../../assets/IntroSound.wav');
+        this.load.audio('Hide', '../../assets/IntroSoundHideText.wav');
     }
 
     create() {
-        this.created = this.time.delayedCall(400, () => {
-            this.createdBy = this.add.text(340, 210, 'Created by:');
-        }, null, this);
-        this.names = this.time.delayedCall(1000, () => {
-            this.nameCredits = this.add.text(200, 230, 'Jarrett Mao, Rishi Prasanna, Felix Tham');
-        }, null, this);
-        this.hideText = this.time.delayedCall(2500, () => {
-            this.createdBy.visible = false;
-            this.nameCredits.visible = false;
-        }, null, this);
-        this.loadMainMenu = this.time.delayedCall(2600, () => {
-            this.scene.start("menuScene");
-        }, null, this);
+        this.press = this.add.text(235, 220, 'Click on the screen to start the game');
+        this.input.on("pointerdown", () => {
+            this.press.visible = false;
+            this.playIntro();
+        });
     }
 
     update() {
 
+    }
+
+    playIntro() {
+        this.created = this.time.delayedCall(400, () => {
+            this.createdBy = this.add.text(340, 210, 'Created by:');
+            this.playSoundText();
+        }, null, this);
+        this.names = this.time.delayedCall(1200, () => {
+            this.nameCredits = this.add.text(200, 230, 'Jarrett Mao, Rishi Prasanna, Felix Tham');
+            this.playSoundText();
+        }, null, this);
+        this.hideText = this.time.delayedCall(2700, () => {
+            this.createdBy.visible = false;
+            this.nameCredits.visible = false;
+            this.playSoundHide();
+        }, null, this);
+        this.loadMainMenu = this.time.delayedCall(3400, () => {
+            this.scene.start("menuScene");
+        }, null, this);
+    }
+    playSoundText() {
+        this.sound.play('Text'); 
+    }
+    playSoundHide() {
+        this.sound.play('Hide'); 
     }
 }
