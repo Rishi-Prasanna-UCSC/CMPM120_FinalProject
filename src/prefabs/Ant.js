d9045dbb0415fcdf5730b60e6467fbbb88023132
“Rishi <riprasan@ucsc.edu>
2021-05-02 14:46:03 -0700

    Renamed creature.png to Spider.png

--- src/prefabs/Ant.js
+++ src/prefabs/Ant.js
@@ -2,6 +2,16 @@ class Ant extends Phaser.GameObjects.Sprite {
     constructor (scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
+
+        // Used for animation.
+        // If false, fall.
+        // If true, run.
+        this.onGround = false;
+
+        // If false, use onGround's status.
+        // If true, swim.
+        // We won't use this for our current state of the Endless Runner.
+        this.inWater = false;
     }
 
     update(){
